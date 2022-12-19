6df153510acf83f271cbd3df54b6a9d376c868fb
Yogi Priyo Prayogo <yogipriyo.prayogo@gmail.com>
2022-12-20 01:40:12 +0700

    setup the bottom tabbar at the homepage

---
+++ ImageMachine/Views/CodeScannerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CodeScannerViewController.swift
+//  ImageMachine
+//
+//  Created by Yogi Priyo Prayogo on 20/12/22.
+//
+
+import UIKit
+
+class CodeScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- ImageMachine/Views/HomepageViewController.swift
+++ ImageMachine/Views/HomepageViewController.swift
@@ -7,12 +7,44 @@
 
 import UIKit
 
-class HomepageViewController: UIViewController {
+class HomepageViewController: UITabBarController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+            
+        setupTabbar()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.delegate = self
+        self.tabBar.tintColor = UIColor.systemBlue
+//        self.tabBar.isTranslucent = false
+    }
+    
+    private func setupTabbar() {
+        let tabOne = MachineListViewController()
+        let tabOneBarItem = UITabBarItem(title: "Machine", image: UIImage(named: "manufacture"), selectedImage: UIImage(named: "selectedImage.png"))
+        tabOne.tabBarItem = tabOneBarItem
+            
+            
+        let tabTwo = CodeScannerViewController()
+        let tabTwoBarItem2 = UITabBarItem(title: "Scan QR", image: UIImage(named: "qr-code"), selectedImage: UIImage(named: "selectedImage2.png"))
+        tabTwo.tabBarItem = tabTwoBarItem2
+            
+        self.viewControllers = [tabOne, tabTwo]
+        
+//        self.tabBar.tintColor = UIColor.white
+//        self.tabBar.isTranslucent = false
     }
 
+}
+
+extension HomepageViewController: UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        print("Selected")
+    }
+    
 }
