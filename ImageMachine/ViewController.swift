e56d893c0c2bf2d2a7faa715541ce57c2710e92e
Yogi Priyo Prayogo <yogipriyo.prayogo@gmail.com>
2022-12-19 23:47:54 +0700

    project barebone

---
+++ ImageMachine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageMachine
+//
+//  Created by Yogi Priyo Prayogo on 19/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
