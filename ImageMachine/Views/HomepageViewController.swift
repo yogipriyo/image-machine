971b065a468c336f7975a25957d6faf427be750d
Yogi Priyo Prayogo <yogipriyo.prayogo@gmail.com>
2022-12-20 00:16:28 +0700

    set the custom root navigation

---
+++ ImageMachine/Views/HomepageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomepageViewController.swift
+//  ImageMachine
+//
+//  Created by Yogi Priyo Prayogo on 20/12/22.
+//
+
+import UIKit
+
+class HomepageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
